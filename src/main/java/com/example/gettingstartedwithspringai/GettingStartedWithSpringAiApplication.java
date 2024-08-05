b798c499d86ef60a473b26d24b774c70b0b680b2
kwasinsiah <truegrayowl@gmail.com>
2024-08-04 23:04:17 -0400

    Project init

---
+++ src/main/java/com/example/gettingstartedwithspringai/GettingStartedWithSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gettingstartedwithspringai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithSpringAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GettingStartedWithSpringAiApplication.class, args);
+    }
+
+}
